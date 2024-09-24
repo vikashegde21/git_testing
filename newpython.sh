89f7a5d549c3049ed7c1fd4d83bdb81c908cc816
random_name <randomemail.com>
2024-09-24 19:50:53 +0000

    a new python script is added

---
+++ newpython.sh
@@ -0,0 +1,10 @@
+import getpass
+
+# Take password from the user
+passwd = getpass.getpass('Password:')
+
+# Check the password
+if passwd == "fahmida":
+    print("You are authenticated")
+else:
+    print("You are not authenticated")
